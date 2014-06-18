ee938dd35501e59973f36513a34e98c22241d0cf
dickeyxxx <jeff@dickeyxxx.com>
2014-06-18 06:25:42 -0700

    mongolab

--- node/db.js
+++ node/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 
-var uri = process.env.MONGOHQ_URL || 'mongodb://localhost/xxx-dev'
+var uri = process.env.MONGOLAB_URI || 'mongodb://localhost/xxx-dev'
 mongoose.connect(uri, function () {
   console.log('connected to', uri)
 })

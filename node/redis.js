2fc0e39a11a9d55fbfd0bc35e054f2f09b2b4722
dickeyxxx <jeff@dickeyxxx.com>
2014-06-18 06:16:48 -0700

    redistogo

--- node/redis.js
+++ node/redis.js
@@ -1,5 +1,12 @@
 var redis = require('redis')
-var client = redis.createClient()
+var client;
+if (process.env.REDISTOGO_URL) {
+  var url = require('url').parse(process.env.REDISTOGO_URL)
+  client = redis.createClient(url.port, url.hostname)
+  client.auth(url.auth.split(':')[1])
+} else {
+  client = redis.createClient()
+}
 
 client.on('error', function (err) {
   console.log(err)

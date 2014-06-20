f52e448dcfd3534ce00f92018ba399f3261447b8
dickeyxxx <jeff@dickeyxxx.com>
2014-06-20 11:39:49 -0700

    only lint in test

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,6 @@ fs.readdirSync('gulp').forEach(function (task) {
   require('./gulp/' + task)
 })
 
-gulp.task('build', ['lint', 'rev'])
+gulp.task('test', ['lint'])
+gulp.task('build', ['rev'])
 gulp.task('default', ['watch:stylus', 'watch:js', 'watch:templates', 'server'])

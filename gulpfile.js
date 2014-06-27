17abd8b4065d355ba83e240987bf74439131db4f
dickeyxxx <jeff@dickeyxxx.com>
2014-06-27 10:16:20 -0700

    use gulp dev instead

--- gulpfile.js
+++ gulpfile.js
@@ -7,4 +7,4 @@ fs.readdirSync('gulp').forEach(function (task) {
 
 gulp.task('test', ['lint'])
 gulp.task('build', ['rev'])
-gulp.task('default', ['watch:stylus', 'watch:js', 'watch:templates', 'server'])
+gulp.task('dev', ['watch:stylus', 'watch:js', 'watch:templates', 'server'])

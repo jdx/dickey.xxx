6fa4640cff683e1bba6b9e4d783708b8d016fbdd
dickeyxxx <jeff@dickeyxxx.com>
2014-06-27 11:36:32 -0700

    reduce number of shown events

--- ng/xxx/githubCtrl.js
+++ ng/xxx/githubCtrl.js
@@ -1,6 +1,6 @@
 angular.module('xxx')
   .controller('GithubCtrl', function ($scope, githubService) {
     githubService.fetchPublicEventsFor('dickeyxxx').success(function (events) {
-      $scope.events = events.splice(0, 15)
+      $scope.events = events.splice(0, 10)
     })
   })

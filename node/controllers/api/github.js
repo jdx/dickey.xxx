7a9ab4e0b73ca3192b080d04f4c3baa61cae8b74
dickeyxxx <jeff@dickeyxxx.com>
2014-06-27 12:05:04 -0700

    cleaner caching of github events

--- node/controllers/api/github.js
+++ node/controllers/api/github.js
@@ -5,17 +5,21 @@ var redis = require('../../redis')
 
 app.get('/github/events', function (req, res, next) {
   var cachekey = 'github:events'
+  var recache = false
   res.setHeader('Cache-Control', 'public, max-age=3600')
   Q.ninvoke(redis, 'get', cachekey)
-  .then(function (events) {
-    if (events) {
-      res.json(JSON.parse(events))
+  .then(function (eventsJson) {
+    if (eventsJson) {
+      return JSON.parse(eventsJson)
     } else {
-      github.fetchPublicEventsFor('dickeyxxx')
-      .then(function (events) {
-        redis.setex(cachekey, 21600, JSON.stringify(events))
-        res.json(events)
-      })
+      recache = true
+      return github.fetchPublicEventsFor('dickeyxxx')
+    }
+  })
+  .then(function (events) {
+    res.json(events)
+    if (recache) {
+      redis.setex(cachekey, 21600, JSON.stringify(events))
     }
   })
   .catch(function (err) { next(err) })
